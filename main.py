8e11ecc745ab103fe58cab468dfd73c64f420a70
Zplattyr <39165549+Zplattyr@users.noreply.github.com>
2024-11-08 14:55:03 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -10,44 +10,18 @@ from fastapi import FastAPI
 from datetime import datetime
 from pydantic import BaseModel
 import asyncio
-import insertData
+from getOnliners import getCountOnliners
 
 
 mutex = asyncio.Lock()
 
 class Passcode(BaseModel):
     passcode: str
+    protocol: str
+    country: str
+    login: bool
 
 metadata = MetaData()
-
-passcodes = Table(
-    "passcodes",
-    metadata,
-    Column("passcode", String, primary_key=True),
-    Column("expiry_date", types.Date)
-)
-
-availables = Table(
-    "availables",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("url", String)
-)
-
-uns = Table(
-    "uns",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("url", String)
-)
-
-users = Table(
-    "users",
-    metadata,
-    Column("id", String, primary_key=True),
-    Column("isAccepted", types.Boolean),
-    Column("passcode", String)
-)
 
 engine = create_engine(
     url = settings.DATABASE_URL_psycopg,
@@ -65,111 +39,69 @@ async def get_key(pasco: Passcode):
     async with mutex:
         with engine.connect() as condata:
             res = condata.execute(text("select * from passcodes")).all()
-
+            query = pasco
             passcodes = [passcode for passcode, date in res]
             dates = [date for passcode, date in res]
-
             pasco = pasco.passcode
-            res2 = condata.execute(text("select * from users where passcode = \'" + pasco + '\'')).all()
-            print(pasco)
-            print(res2)
+            if len(pasco) > 10:
+                return "!INVALID "
+            res2 = condata.execute(text('select * from users_passcodes where "passcode" = \'' + pasco + '\'')).all()
+            print("Passcode: ", pasco)
+            print("SQL: ", res2)
             if pasco in  passcodes:
                 passdate = datetime.strptime(dates[passcodes.index(pasco)], "%Y-%m-%d")
                 today = datetime.today()
+                if pasco in onlinersid:
+                    if onlinersid[pasco] + 5 >= time.time():
+                        return "!WAIT "
                 if passdate >= today:
                     res = condata.execute(text("select * from availables")).all()
-                    keys = [key for _, key in res]
+                    keys:list[str] = [key for _, key in res]
                     for key in keys:
-                        if key not in onlinerspass:
-                            return str(key) + ' ' + res2[0][0]
-                        else:
-                            if time.time() - onlinerspass[key] >= 30:
+                        if (query.country and key.find(query.country) == -1):
+                            continue
+                        if (query.protocol and key.find(query.protocol) == -1):
+                            continue
+                        count_onliners = 0
+                        if(not query.login):
+                            count_onliners = getCountOnliners(key, engine)
+                        print("Count: ", count_onliners)
+                        if(count_onliners > 3):
+                            continue
+                        try:
+                            if key not in onlinerspass:
                                 return str(key) + ' ' + res2[0][0]
                             else:
-                                continue
+                                if time.time() - onlinerspass[key] >= 30:
+                                    return str(key) + ' ' + res2[0][0]
+                                else:
+                                    continue
+                        except:
+                            return "!INVLAID "
                 else:
                     return "!INVALID "
             else:
                 return "!INVALID "
 
 
-
-
 @app.post("/onlinepass")
 async def is_online(pasco: Passcode):
     async with mutex:
         onlinerspass[pasco.passcode] = time.time()
-        print(onlinerspass)
 
 @app.post("/onlineid")
 async def is_online(pasco: Passcode):
     async with mutex:
         onlinersid[pasco.passcode] = time.time()
-        print(onlinersid)
-#
-# HEADER = 64
-# PORT = 5050
-# SERVER = socket.gethostbyname(socket.gethostname())
-# ADDR = (SERVER, PORT)
-# FORMAT = 'utf-8'
-#
-# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# server.bind(ADDR)
-#
-# engine = create_engine(
-#     url = settings.DATABASE_URL_psycopg,
-#     echo=False,
-#     pool_size=5,
-#     max_overflow=10
-# )
-#
-# avails1 = set()
-# avails2 = set()
-#
-#
-# my_mutex = threading.Lock()
-# def handle_client(conn, addr):
-#     print(f"[NEW CONNECTION] {addr} connected")
-#
-#     msg = conn.recv(1024).decode(FORMAT)
-#     print(f"[{addr}] {msg}")
-#     msg = msg.split()
-#
-#     if(msg[0] != '!PASSCODE'):
-#         conn.close()
-#
-#     my_mutex.acquire()
-#     with engine.connect() as condata:
-#         res = condata.execute(text("select * from passcodes")).all()
-#         passcodes = [passcode for passcode, date in res]
-#         dates = [date for passcode, date in res]
-#         print(passcodes)
-#
-#     if (msg[1] not in passcodes):
-#         conn.send("!INVALID".encode('utf-8'))
-#         conn.close()
-#         my_mutex.release()
-#         return
-#
-#     with engine.connect() as condata:
-#         res = condata.execute(text("select * from availables"))
-#         key = res.all()[0][1]
-#         print(f"{key}")
-#         insertData.InsertUrl(engine,uns,key)
-#         insertData.DeleteUrl(engine,availables,key)
-#
-#     conn.send(key.encode('utf-8'))
-#
-#     my_mutex.release()
-#     conn.close()
-#
-# def start():
-#     server.listen()
-#     while True:
-#         conn, addr = server.accept()
-#         thread = threading.Thread(target=handle_client, args=(conn, addr))
-#         thread.start()
-#         print(f"[ACTIVE CONNETCIONS] {threading.active_count() - 1}")
-#
-# print('starting...')
-# start()
+
+
+async def print_clients():
+    while True:
+        async with mutex:
+            print(onlinersid)
+            print(onlinerspass)
+        await asyncio.sleep(10)
+
+@app.on_event("startup")
+async def on_startup():
+    asyncio.create_task(print_clients())
