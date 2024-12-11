69c5823888a88f7d4d8d4cfca703091a7214a8d0
Zplattyr <39165549+Zplattyr@users.noreply.github.com>
2024-12-11 12:20:04 +0300

    Add files via upload

--- resetUrls.py
+++ resetUrls.py
@@ -79,16 +79,12 @@ async def resetUrl(url:str, engine, mutex, stop_event, onlinerskey):
                     pbk = json.loads(indata['streamSettings'])['realitySettings']['settings']['publicKey']
                     sid = json.loads(indata['streamSettings'])['realitySettings']['shortIds'][0]
                     key = addVless(host, main_port, indata['port'], panel, username, password, id, pbk, sid, server)
-                    print('addvless', key)
                     stop_event.set()
                     async with mutex:
                         AddToAvailables(engine, key)
-                        print('addtoavail', key)
                         DeleteFromAvailables(engine, url)
-                        print('deletefromavail', url)
                     stop_event.clear()
                     deleteVless(host, main_port, panel, username, password, id, client['id'])
-                    print('deletevless', url)
                 # elif server.find('shadowsocks') != -1:
                 #     # print(indata)
                 #     security = json.loads(indata['settings'])['method']
