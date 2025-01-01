0e062548d10739af06fec2d6681a476c3b7d0606
Zplattyr <39165549+Zplattyr@users.noreply.github.com>
2025-01-01 21:46:13 +0300

    Add files via upload

--- data.py
+++ data.py
@@ -7,31 +7,31 @@ class SafeDict:
         self._map = dict()
 
     def __setitem__(self, key, value):
-        async with self._mutex:
+        with self._mutex:
             self._map[key] = value
 
     def __getitem__(self, key):
-        async with self._mutex:
+        with self._mutex:
             return self._map[key]
 
     def __contains__(self, key):
-        async with self._mutex:
+        with self._mutex:
             return key in self._map
 
     def __len__(self):
-        async with self._mutex:
+        with self._mutex:
             return len(self._map)
 
     def __str__(self):
-        async with self._mutex:
+        with self._mutex:
             return str(self._map)
 
     def keys(self):
-        async with self._mutex:
+        with self._mutex:
             return self._map.keys()
 
     def pop(self, key):
-        async with self._mutex:
+        with self._mutex:
             self._map.pop(key)
 
 
