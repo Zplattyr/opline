d2ca9667541c487511b72629a56476c298daec47
Zplattyr <39165549+Zplattyr@users.noreply.github.com>
2025-01-01 22:49:44 +0300

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,32 +1,3 @@
+from datetime import datetime
 
-app = FastAPI()
-
-
-
-@app.post("/get_key")
-async def get_key(pasco: Passcode):
-    ...
-
-
-@app.post("/onlinepass")
-async def is_online(pasco: Passcode):
-    ...
-
-@app.post("/onlinekey")
-async def is_online(pasco: Passcode):
-    ...
-
-
-async def print_clients():
-    while True:
-        ...
-
-async def delete_online_keys():
-    while True:
-        ...
-
-@app.on_event("startup")
-async def on_startup():
-    asyncio.create_task(print_clients())
-    asyncio.create_task(delete_online_keys())
-    asyncio.create_task(getAndResetUrls(engine, stop_event))
+print(datetime.fromtimestamp(48776))
